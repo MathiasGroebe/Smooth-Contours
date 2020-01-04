162c7ee60d0a5ba1660ee907d98722c19c752247
Mathias Gröbe <mathias.groebe@gmail.com>
2020-01-04 11:50:19 +0100

    First sample

--- create_contours.py
+++ create_contours.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 
-print("Hello world!")
+import os
+
+dgmFile = "334245636_dgm1.tif"
+intervall = 10
+name = "contour"
+
+os.system("gdal_contour -inodata -snodata -32768 -a ele "  + dgmFile + " " + name + ".sqlite -i " + str(intervall))
